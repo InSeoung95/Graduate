2e4c54754cfb84400ca22239e441055547c8b39b
YSH <2016180054@kpu.ac.kr>
2020-01-08 13:11:27 +0900

    1인칭시점,시야설정

---
+++ Escape/Assets/Script/CheckObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //오브젝트 이동
+        //transform.Translate(Vector3.right * Time.deltaTime);
+
+
+        //오브젝트가 카메라 안에 있는지 확인
+        // Debug.Log(Checking(gameObject));
+    }
+
+    public bool Checking(GameObject target)
+    {
+        Camera selectedCameta = GameObject.Find("Main Camera").GetComponent<Camera>(); //Main Camera 카메라 이름 적기
+        Vector3 screenPoint = selectedCameta.WorldToViewportPoint(target.transform.position);
+        //screenPoint 0~1 사이의 값을 주어 제한해서 시야를 정해줄수있음
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0.3 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        return onScreen;
+    }
+}
---
+++ Escape/Assets/Script/MainController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    //이동
+    public Animator animator;
+    public float speed = 10;
+    private float h; // 좌,우
+    private float v; // 상,하
+
+    //시야전환
+    public Camera cam;
+    public float rotateSpeed = 5f;
+    public float cameraRotationLimit = 80f;
+    private float cameraRotation = 0f;
+    private float currentCameraRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        movePlayer();
+        eyeRotation();
+
+    }
+
+    void eyeRotation()  //x,y 회전(시야회전)
+    {
+
+        float rotX = Input.GetAxis("Mouse Y") * rotateSpeed;    
+        float rotY = Input.GetAxis("Mouse X") * rotateSpeed;
+
+        this.transform.localRotation *= Quaternion.Euler(0, rotY, 0);
+        cam.transform.localRotation *= Quaternion.Euler(-rotX, 0, 0);
+
+        cameraRotation = rotX;
+
+        if (cam != null)
+        {
+            currentCameraRotation -= cameraRotation;
+            currentCameraRotation = Mathf.Clamp(currentCameraRotation, -cameraRotationLimit, cameraRotationLimit);
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotation, 0f, 0f);
+        }
+    }
+
+    void movePlayer() 
+    {
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        //Time.smoothDeltaTime cpu마다 성능이 달라서 곱해준다.
+        if (Input.GetKey("a"))
+        {
+            transform.position = new Vector3(transform.position.x - (speed * Time.smoothDeltaTime), transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey("d"))
+        {
+            transform.position = new Vector3(transform.position.x + (speed * Time.smoothDeltaTime), transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - (speed * Time.smoothDeltaTime));
+        }
+        else if (Input.GetKey("w"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (speed * Time.smoothDeltaTime));
+        }
+ 
+    }
+}
+
+
